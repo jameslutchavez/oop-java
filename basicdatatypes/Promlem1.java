43b1fac270ac7936c2673b6c924b07c3d6d2c2c2
jameslutchavez <jamespelaezlutchavez@gmail.com>
2025-08-27 14:26:17 +0800

    Add files via upload

---
+++ basicdatatypes/Promlem1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicdatatypes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Promlem1 {
+    
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Input a degree in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("%.1f degrees Fahrenheit is equal to %.1f in Celsius%n", fahrenheit, celsius);
+    }
+}
+        
+
+
+    
